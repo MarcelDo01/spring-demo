23415def2ecfec5abb37d46004f1c639075aa97d
MarcelDo01 <s0578647@htw-berlin.de>
2023-04-23 12:45:21 +0200

    1. Meilenstein: "Hello World" Spring Boot App erstellt

---
+++ src/main/java/de/berlin/htw/webtech/demo/CreatingController.java
@@ -0,0 +1,16 @@
+package de.berlin.htw.webtech.demo;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreatingController {
+
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name",required = false, defaultValue = "World" ) String name) {
+        return  "Hello " + name;
+    }
+}
