14f4f640e9050d53b146e88be61de95194465391
MarcelDo01 <s0578647@htw-berlin.de>
2023-05-20 17:27:16 +0200

    Clean up Repository

--- src/main/java/m2/Controller/ThingController.java
+++ src/main/java/m2/Controller/ThingController.java
@@ -1,5 +1,7 @@
-package Objects;
+package m2.Controller;
 
+import m2.Service.ThingService;
+import m2.Thing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
